c782905f16964f20d6fb6c85e15ee2999945c7b5
ZhengLI-git <zhengli.2333@gmail.com>
2020-11-15 18:45:20 +0100

    reneweverything

--- cnn.py
+++ cnn.py
@@ -1,4 +1,6 @@
 import pandas
+import tensorflow as tf
+from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
@@ -6,8 +8,8 @@ from keras.layers import Dense
 def ourCNN():
 	# create model
 	model = Sequential()
-	model.add(Dense(60, input_dim=60, activation='relu'))
-	model.add(Dense(30, activation='relu'))
+	model.add(Dense(4, input_dim=4, activation='relu'))
+	model.add(Dense(16, activation='relu'))
 	model.add(Dense(1, activation='sigmoid'))
 	# Compile model
 	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
