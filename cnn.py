e196276cc59c08c311841f1a89e29b6448709004
ZhengLI-git <zhengli.2333@gmail.com>
2020-11-15 15:04:27 +0100

    simple CNN

---
+++ cnn.py
@@ -0,0 +1,14 @@
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+def CNN():
+	# create model
+	model = Sequential()
+	model.add(Dense(60, input_dim=60, activation='relu'))
+	model.add(Dense(30, activation='relu'))
+	model.add(Dense(1, activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
